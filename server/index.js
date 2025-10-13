50d2e7107e701f8378875e11b15e64373b9d616b
Aastha Jain <aasthajain7499@gmail.com>
2025-10-13 21:19:42 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -212,6 +212,8 @@ io.on("connection", (socket) => {
 });
 
 
+// MongoDB & server start
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
