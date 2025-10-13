19529ba566a70d5be0152992eaeddb731d15ba73
Aastha Jain <aasthajain7499@gmail.com>
2025-10-13 21:47:43 +0530

    Updated main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,13 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import { AuthLayout, Login } from './components/index.js'
+import { AuthLayout} from './components/index.js'
 
 
 import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
+import SignUp from './pages/Signup'
 import EditPost from "./pages/EditPost";
-
+import Login from './pages/Login'
 import Post from "./pages/Post";
 
 import AllPosts from "./pages/AllPosts";
