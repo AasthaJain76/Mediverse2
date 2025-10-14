130d364db57c1d78ba9b6f142f3b0283e30150bb
Aastha Jain <aasthajain7499@gmail.com>
2025-10-14 20:38:29 +0530

    UPdated

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://mediverse2-9.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://mediverse2-9.onrender.com",
   withCredentials: true, // ✅ Required for cookies
 });
 
