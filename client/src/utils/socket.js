4b2e7e61d13e00982be89296d1f97d668806360c
Aastha Jain <aasthajain7499@gmail.com>
2025-10-14 20:02:50 +0530

    Updated

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,7 @@
 // src/utils/socket.js
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || "https://mediverse2-9.onrender.com/";
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || "https://mediverse2-9.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
